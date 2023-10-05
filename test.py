09c42687f2ef0365e1ad9ea32e44dc3a7bed7f72
Ranzan_998 <ranzan998@gmail.com>
2023-10-05 15:21:11 +0530

    new file:   scripts_techniques_imp.py 	new file:   test.py

---
+++ test.py
@@ -0,0 +1,13 @@
+# Define two arrays
+array1 = [1, 2, 3, 4, 5]
+array2 = [6, 7, 8, 9, 10]
+
+# Check if the arrays have the same length
+if len(array1) == len(array2):
+    # Use list comprehension to add corresponding elements
+    result = [array1[i] + array2[i] for i in range(len(array1))]
+else:
+    print("Arrays have different lengths.")
+
+# Print the result
+print(result)
